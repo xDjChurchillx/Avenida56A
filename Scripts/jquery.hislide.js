ecebe2f25225e7e6fec9eafe5fa8b3deab887186
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2024-05-11 18:04:58 -0600

    Update jquery.hislide.js

--- Scripts/jquery.hislide.js
+++ Scripts/jquery.hislide.js
@@ -14,7 +14,7 @@
             //{ $zIndex: 3, width: 170, height: 218, top: 35, left: 470, $opacity: 0.7 },
             //{ $zIndex: 2, width: 130, height: 170, top: 59, left: 620, $opacity: 0.4 },
             //{ $zIndex: 1, width: 120, height: 150, top: 69, left: 500, $opacity: 0.2 }
-            { $zIndex: 1, width: 12 + 'vw', height: 15 + 'vw', top: 0, left: 21 + '%', $opacity: 0.8 },
+            { $zIndex: 1, width: 12 + 'vw', height: 15 + 'vw', top: 0, left: 21 + '%', $opacity: 0.999999 },
             { $zIndex: 2, width: 12 + 'vw', height: 15 + 'vw', top: 0, left: 0,        $opacity: 1 },
             { $zIndex: 3, width: 12 + 'vw', height: 15 + 'vw', top: 0, left: 21 + '%', $opacity: 1 },
             { $zIndex: 4, width: 12 + 'vw', height: 15 + 'vw', top: 0, left: 43 + "%", $opacity: 1 },
