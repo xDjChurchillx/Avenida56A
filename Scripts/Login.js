313a7521a2cefb6dd9a75e6b1e6ac39dc1a23b87
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2024-05-23 15:54:49 -0600

    Update Login.js

--- Scripts/Login.js
+++ Scripts/Login.js
@@ -91,22 +91,32 @@ function cambiar() {
         if (correoFinal === "") {
             correoError3.textContent = "Por favor, ingresa un correo electr\u00F3nico v\u00E1lido.";
             habilitarEnvio(); // Habilitar el envío de la solicitud
+            var correorec2 = document.getElementById("correoFinal");
+            correorec2.focus();
             return false;
         } else if (!/^\w+@\w+.com$/.test(correoFinal)) {
             correoError3.textContent = "Por favor, ingresa un correo electr\u00F3nico v\u00E1lido.";
             habilitarEnvio(); // Habilitar el envío de la solicitud
+            var correorec2 = document.getElementById("correoFinal");
+            correorec2.focus();
             return false;
         } else if (contraFinal === "") {
             pswrdError.textContent = "Por favor, ingresa una contrase\u00F1a v\u00E1lida.";
             habilitarEnvio(); // Habilitar el envío de la solicitud
+            var correorec2 = document.getElementById("correoFinal");
+            correorec2.focus();
             return false;
         } else if (contraFinal.length > 50) {
             pswrdError.textContent = "La contrase\u00F1a no puede tener m\u00E1s de 50 caracteres.";
             habilitarEnvio(); // Habilitar el envío de la solicitud
+            var correorec2 = document.getElementById("correoFinal");
+            correorec2.focus();
             return false;
         } else if (contraFinal.length < 8) {
             pswrdError.textContent = "La contrase\u00F1a no puede tener menos de 8 caracteres.";
             habilitarEnvio(); // Habilitar el envío de la solicitud
+            var correorec2 = document.getElementById("correoFinal");
+            correorec2.focus();
             return false;
         } else {
             correoError3.textContent = "";
