b3a09ed999f93735c5e5ee3d17547a8df226ed6e
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2024-05-22 23:58:15 -0600

    Update Proc_Recuperar.php

--- Account/Proc_Recuperar.php
+++ Account/Proc_Recuperar.php
@@ -6,10 +6,7 @@ require '../../Private/Credentials/DataBase/connection.php';
     
         $correo = isset($_GET['correo']) ? $_GET['correo'] : '';
 
-        if ($conn8->connect_error || $conn8 === null) {
-                
-            }     
-         
+       
             $queryListar = "CALL Cte_ListCorreos('$correo')";
             $result = mysqli_query($conn8, $queryListar);
             if ($result === false) {
