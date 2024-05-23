df13af4b7057f4e10f047284a3822198152257bf
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2024-05-22 23:48:34 -0600

    Update Proc_Recuperar.php

--- Account/Proc_Recuperar.php
+++ Account/Proc_Recuperar.php
@@ -6,12 +6,33 @@ require '../../Private/Credentials/DataBase/connection.php';
     
         $correo = isset($_GET['correo']) ? $_GET['correo'] : '';
 
-        echo $correo;
+        if ($conn8->connect_error || $conn8 === null) {
+                 echo "-1";
+            }
+         
+            $queryListar = "CALL Cte_ListCorreos('$correo')";
+            $result = mysqli_query($conn8, $queryListar);
+            if ($result === false) {
+                echo "-1";
+            } else {
+                $email_exists = false;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    // Verificar si el correo actual es igual al recibido en el formulario
+                    if ($row['Correo'] == $correo) {
+                        $email_exists = true;
+                        break; // Salir del bucle ya que hemos encontrado una coincidencia
+                    }
+                }
+       
+         // Mostrar mensaje dependiendo de si se encontró el correo o no
+                if ($email_exists) {
+                  echo "1";  
+                } else {
+                   echo "-1";                            
+                }
            
 
     } catch (Exception $ex) {
-        echo "<script>alert('Se produjo un error: " . $ex->getMessage() . "');
-             window.location.href = 'Register.html';</script>";
-        exit();
+         echo "-1";
     }
     ?>
