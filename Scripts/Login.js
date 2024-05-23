3e3d5be72980acd96a6828a7ce0260ec87196cfd
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2024-05-23 02:32:35 -0600

    Update Login.js

--- Scripts/Login.js
+++ Scripts/Login.js
@@ -16,7 +16,7 @@ let filtro = extraerFiltroDeUrl(location.search);
 
 function extraerFiltroDeUrl(url) {
     console.log(url);
-    var filtroIndex = url.indexOf('?filter=');
+    var filtroIndex = url.indexOf('filter=');
     console.log(filtroIndex);
     return filtroIndex;
 }
