848ca624859a4d634654d723b59fff7166f7c1a7
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2024-05-30 17:43:12 -0600

    Update Proc_Contact.php

--- Administration/Proc_Contact.php
+++ Administration/Proc_Contact.php
@@ -11,9 +11,9 @@ require '../PHPMailer/SMTP.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {    
 
     try {
-        // Instancia un nuevo objeto PHPMailer
-        $mail = new PHPMailer(true);
         
+          // Instancia del encriptador
+        $encriptador = new Encriptador();
 
 
         // Recibir y limpiar los datos del formulario
