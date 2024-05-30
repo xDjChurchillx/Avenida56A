4649c688da2b6e76b90df1cfe59a6fc2a7d1e61a
xDjChurchillx <158157933+xDjChurchillx@users.noreply.github.com>
2024-05-30 17:16:09 -0600

    Update Proc_Contact.php

--- Administration/Proc_Contact.php
+++ Administration/Proc_Contact.php
@@ -15,24 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $encriptador = new Encriptador();
 
     try {
-        // Recibir y limpiar los datos del formulario
-        $nombre = htmlspecialchars($_POST["nombre"]);
-        $correo = htmlspecialchars($_POST["correo"]);
-        $asunto = htmlspecialchars($_POST["asunto"]);
-        $mensaje = htmlspecialchars($_POST["mensaje"]);
-        $responder = isset($_POST["responder"]) ? "True" : "False";
-       
-
-        $nombre = trim($nombre);
-        $correo = trim($correo);
-        $asunto = trim($asunto);
-
-            if ($conn10->connect_error || $conn10 === null) {
-                echo "<script>alert('La base de datos ha fallado');
-                 window.location.href = '/index.html';</script>";            
-                exit();
-            }
-           
+     
            echo "<script>alert('hh');</script>";
       
 
